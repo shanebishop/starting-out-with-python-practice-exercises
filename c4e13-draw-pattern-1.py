1bd7c7f8bcad67d40afa8099af34cbcdb0072605
shanebishop <shane.bishop@outlook.com>
2016-11-26 15:10:23 -0500

    Add files via upload

---
+++ c4e13-draw-pattern-1.py
@@ -0,0 +1,4 @@
+for x in range(7, 0, -1):
+	for y in range(x, 0, -1):
+		print("*", end="")
+	print()
---
+++ c4e14-draw-pattern-2.py
@@ -0,0 +1,5 @@
+for x in range(6):
+	print("#", end="")
+	for y in range(x, 0, -1):
+		print(" ", end="")
+	print("#")
